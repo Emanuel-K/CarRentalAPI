704894e2998cbb664a04d5aa425366e447a1a3d5
Danihabtu <133099871+Danihabtu@users.noreply.github.com>
2025-01-29 19:18:33 +0300

    Create IRentalService.cs

---
+++ Services/IRentalService.cs
@@ -0,0 +1,12 @@
+using CarRentalAPI.Models;
+namespace CarRentalAPI.Services{
+  public interface IRentalService{
+        Task<List<Rental>> GetAsync();
+        Task<Rental> GetAsync(int id);
+        Task CreateAsync(Rental rental);
+        Task UpdateAsync(int id, Rental rental);
+        Task DeleteAsync(int id);
+        Task<List<Rental>> SearchRentalsAsync(string searchTerm);
+  }}
+using CarRentalAPI.Models;
+
