39b149cdc66d90316fe45ae4cb690ad0eebf61ad
Danihabtu <133099871+Danihabtu@users.noreply.github.com>
2025-01-30 00:31:25 +0300

    dependency injection and add controllers

--- Program.cs
+++ Program.cs
@@ -9,3 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.Configure<MongoDBSettings>(
     builder.Configuration.GetSection("MongoDBSettings"));
+builder.Services.AddSingleton<ICarService, CarService>();
+builder.Services.AddSingleton<IRentalService, RentalService>();
+
+// Add Controllers and Configure Validation
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true; // Use custom validation if needed
+    });
